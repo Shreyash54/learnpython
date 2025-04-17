28601f879580c159de70566b2521ef8eb19c5af8
Shreyash Gupta <91052115+Shreyash54@users.noreply.github.com>
2025-04-18 02:22:08 +0530

    Update and rename Prob8 to Prob8.py

--- Leetcode/Prob8.py
+++ Leetcode/Prob8.py
@@ -1,4 +1,4 @@
- Reverse Nodes in k-Group
+''' Reverse Nodes in k-Group
 Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 
 k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
@@ -6,7 +6,7 @@ k is a positive integer and is less than or equal to the length of the linked li
 You may not alter the values in the list's nodes, only nodes themselves may be changed.
 
 code:
-
+'''
 
 
 # Definition for singly-linked list.

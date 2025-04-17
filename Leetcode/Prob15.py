e4151aeb892312fd3f1595dc4de059077d6843c3
Shreyash Gupta <91052115+Shreyash54@users.noreply.github.com>
2025-04-18 02:24:32 +0530

    Update and rename Prob15 to Prob15.py

--- Leetcode/Prob15.py
+++ Leetcode/Prob15.py
@@ -1,4 +1,4 @@
-You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+'''You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
 Find two lines that together with the x-axis form a container, such that the container contains the most water.
 
@@ -18,7 +18,7 @@ Output: 1
 
 https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg
 
-code:
+code:'''
 class Solution:
     def maxArea(self, height: List[int]) ->int:
         max_area=0

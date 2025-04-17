af520b039f2442be51fa53da06563104d593a82a
Shreyash Gupta <91052115+Shreyash54@users.noreply.github.com>
2025-04-18 02:24:52 +0530

    Update and rename Prob16 to Prob16.py

--- Leetcode/Prob16.py
+++ Leetcode/Prob16.py
@@ -1,4 +1,4 @@
-merge alternatively
+'''merge alternatively
 You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
 Return the merged string.
@@ -20,6 +20,7 @@ word2:    p   q   r   s
 merged: a p b q   r   s
 
 code:
+'''
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         res=[]
